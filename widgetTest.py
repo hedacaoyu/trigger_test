08002838286ba84b2777e23de031f91c4a1637f3
caochuanyu <caochuanyu@u51.com>
2018-09-15 00:55:36 +0800

    add incorrect

--- widgetTest.py
+++ widgetTest.py
@@ -7,7 +7,7 @@ class WidgetTestCase(unittest.TestCase):
         self.widget.dispose()
         self.widget = None
     def testSize(self):
-        self.assertEqual(self.widget.getSize(), (40, 40))
+        self.assertEqual(self.widget.getSize(), (40, 41))
     def testResize(self):
         self.widget.resize(100, 100) 
         self.assertEqual(self.widget.getSize(), (100, 100)) 
